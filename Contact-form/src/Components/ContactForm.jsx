e07351062ee3e54b420270624a4564be20ad2050
SODA GUY <ammarisahmed@gmail.com>
2026-02-08 23:39:07 +0400

    finalized contact us form

--- Contact-form/src/Components/ContactForm.jsx
+++ Contact-form/src/Components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ContactForm.css';
-import contactImage from  '/contactus.jpg';
+import contactImage from  '/contactus.png';
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: '',
