79d0a7fc87cb50d2cab6d3b325e538765150a6ee
SODA GUY <ammarisahmed@gmail.com>
2026-02-27 16:58:08 +0400

    Added files and mock data

--- Freelance/src/pages/MainDashboard.jsx
+++ Freelance/src/pages/MainDashboard.jsx
@@ -1,7 +1,13 @@
+import MockData from '../data/MockData';
+
 const MainDashboard = () => {
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold text-gray-900">Main Dashboard</h1>
+            <p className="text-gray-600 mt-2"> Total Projects: {MockData.stats.totalProjects}</p>
+            <p className="text-gray-600 mt-1"> Monthly Goal: ${MockData.stats.MonthlyGoal}</p>
+            <p className="text-gray-600 mt-1"> Active Clients: {MockData.stats.activeClients}</p>
+            <p className="text-gray-600 mt-1"> Earnings This Month: ${MockData.stats.earnings}</p>
         </div>
     )
 }   
